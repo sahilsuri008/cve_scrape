ebdd22093fb9dda2057537252ff046ea3a033079
sahilsuri008 <34906648+sahilsuri008@users.noreply.github.com>
2021-11-02 16:13:18 +0530

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+r = requests.get('https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=junos+12.3R12-S15')
+
+c = r.content
+
+soup = BeautifulSoup(c, "html.parser")
+
+#print (soup)
+
+main_content = soup.find('div', attrs = {'id': 'TableWithRules'})
+content = main_content.find('table').text
+print (content)
