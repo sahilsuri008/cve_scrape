e4e899e28d530ef2b0fe69d81c2646bc0d561018
sahilsuri008 <34906648+sahilsuri008@users.noreply.github.com>
2021-11-04 01:26:08 +0530

    Saved o/p to file and formatted file

--- scrape.py
+++ scrape.py
@@ -3,13 +3,25 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+import subprocess
 
-r = requests.get('https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=junos+12.3R12-S15')
+#remove old output file#
+subprocess.call("rm -rf myfile.txt", shell=True)
 
-c = r.content
+os_versions = [ "12.3R12-S15", "20.2R3-S2" ]
 
-soup = BeautifulSoup(c, "html.parser")
+for os_version in os_versions:
+    r = requests.get('http://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=junos+{}'.format(os_version))
+    c = r.content
 
-main_content = soup.find('div', attrs = {'id': 'TableWithRules'})
-content = main_content.find('table').text
-print (content)
+    soup = BeautifulSoup(c, "html.parser")
+
+    main_content = soup.find('div', attrs = {'id': 'TableWithRules'})
+    content = main_content.find('table').text
+    file1 = open("myfile.txt", "a")
+    file1.write("Printing CVE details for Junos version:"+ os_version + "\n")
+    file1.write(content)
+    file1.close()
+
+subprocess.call("sed -i '/^.*CVE/{N;s/\\n */, /}' myfile.txt", shell=True)
+subprocess.call("sed -i '/^.*Name/{N;s/\\n */, /}' myfile.txt", shell=True)
