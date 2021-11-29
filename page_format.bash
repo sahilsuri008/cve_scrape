f6627404b2272c0d5be6db5bab7c2001233bb5be
sahilsuri008 <34906648+sahilsuri008@users.noreply.github.com>
2021-11-29 12:02:59 +0530

    Update page_format.bash

--- page_format.bash
+++ page_format.bash
@@ -26,3 +26,53 @@ sed -i '$e cat vul.txt' page_data_open
 sed -i '$e cat resolved_page.txt' page_data_resolved
 
 rm -rf active_cve.txt resolved_cve.txt overall_cve.txt unique_cve.txt resolved_page.txt
+
+#Separate active and resovled CVEs for Cisco#
+awk -F '<td>' '{print $3}' page_data_open_cisco | tr -d '</td>' | grep CVE- >> active_cve_cisco.txt
+awk -F '<td>' '{print $3}' page_data_resolved_cisco | tr -d '</td>' | grep CVE- >> resolved_cve_cisco.txt
+
+cat active_cve_cisco.txt resolved_cve_cisco.txt >> overall_cve_cisco.txt
+sort -u overall_cve_cisco.txt >> unique_cve_cisco.txt
+
+#Remove CVEs already on the page#
+for vul in $(<unique_cve_cisco.txt); do
+ sed -i "/$vul/d" results_cisco.txt
+done
+
+#Remove resolved CVEs from open CVE report page#
+grep -i resolved page_data_open_cisco >> resolved_page_cisco.txt
+
+#Remove Resolved CVEs from Open CVE page#
+sed -i "/resolved/Id" page_data_open_cisco
+
+## Input into page_data file##
+
+sed -i '$e cat results_cisco.txt' page_data_open_cisco
+sed -i '$e cat resolved_page_cisco.txt' page_data_resolved_cisco
+
+rm -rf active_cve_cisco.txt resolved_cve_cisco.txt overall_cve_cisco.txt unique_cve_cisco.txt resolved_page_cisco.txt
+
+#Separate active and resovled CVEs for F5#
+awk -F '<td>' '{print $3}' page_data_open_f5 | tr -d '</td>' | grep CVE- >> active_cve_f5.txt
+awk -F '<td>' '{print $3}' page_data_resolved_f5 | tr -d '</td>' | grep CVE- >> resolved_cve_f5.txt
+
+cat active_cve_f5.txt resolved_cve_f5.txt >> overall_cve_f5.txt
+sort -u overall_cve_f5.txt >> unique_cve_f5.txt
+
+#Remove CVEs already on the page#
+for vul in $(<unique_cve_f5.txt); do
+ sed -i "/$vul/d" results_f5.txt
+done
+
+#Remove resolved CVEs from open CVE report page#
+grep -i resolved page_data_open_f5 >> resolved_page_f5.txt
+
+#Remove Resolved CVEs from Open CVE page#
+sed -i "/resolved/Id" page_data_open_f5
+
+## Input into page_data file##
+
+sed -i '$e cat results_f5.txt' page_data_open_f5
+sed -i '$e cat resolved_page_f5.txt' page_data_resolved_f5
+
+rm -rf active_cve_f5.txt resolved_cve_f5.txt overall_cve_f5.txt unique_cve_f5.txt resolved_page_f5.txt
